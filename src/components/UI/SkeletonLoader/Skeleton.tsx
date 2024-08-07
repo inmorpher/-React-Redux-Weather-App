2f825995ddd0dbfa010123649a3988dcbbba0d7d
inmorpher <omennnn62@gmail.com>
2024-08-07 18:42:40 -0400

    [Update](Favicon): Updated favicon icon

--- src/components/UI/SkeletonLoader/Skeleton.tsx
+++ src/components/UI/SkeletonLoader/Skeleton.tsx
@@ -1,14 +1,5 @@
 const Skeleton = () => {
-	// return (
-	// 	<div className='relative flex h-full w-full flex-col overflow-hidden'>
-	// 		<div className='flex items-center gap-4 p-1'>
-	// 			<div className='w-1/3 rounded-full bg-black/[.50]' />
-	// 			<div className='h-[10%] w-[55%] rounded-xl bg-black/[.50]' />
-	// 		</div>
-	// 		<div className='h-1/5 w-full rounded bg-black/[.50]' />
-	// 	</div>
-	// );
-	return <div className='w-[95%] h-[95%] bg-black rounded-lg animate-pulse m-2'></div>;
+	return <div className='w-[95%] h-[95%] bg-weather-bg-900 rounded-lg animate-pulse m-2'></div>;
 };
 
 export default Skeleton;
