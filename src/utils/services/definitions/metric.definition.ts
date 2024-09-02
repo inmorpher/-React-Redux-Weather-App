3fa4b4646a739c3741be224446084f8e1dcb97a4
inmorpher <omennnn62@gmail.com>
2024-09-02 19:53:31 -0400

    [Deleted]: Deleted uneccecary files - src/components/UI/Loader/Loader.module.scss; - src/utils/services/definitions/air.definition.ts; - src/utils/services/definitions/metric.definition.ts

--- src/utils/services/definitions/metric.definition.ts
+++
@@ -1,4 +0,0 @@
-import { MetricType } from '../../../context/Metric.context';
-
-export const getMetricTempriture = (value: number, metric: MetricType) =>
-	metric === 'metric' ? Math.round(value - 273.15) : Math.round(((value - 273.15) * 9) / 5 + 32);
